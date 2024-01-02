2f9efb981f4676bf788ed3935911b0ed10066075
İsmail Tezal <ismailtezal33@gmail.com>
2024-01-02 05:02:18 +0300

    fix: improved seo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="Flight search application for finding the best flight deals." />
+        <meta name="keywords" content="flight search, flight deals, travel" />
+        <meta name="author" content="Your Name" />
+        <title>Flight Search Application</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
